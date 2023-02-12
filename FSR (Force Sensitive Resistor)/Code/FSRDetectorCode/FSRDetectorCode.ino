776fc3de099fdc18b6c42e765396667929a1c9b3
Monish Saravana <monish.sd.2008@Gmail.com>
2023-02-12 00:24:53 -0500

    Updated to yale code
    - Added the code used for yale
    - based on the most up to date readings - uses a piecewise function for conversion of voltage to grams

--- FSR (Force Sensitive Resistor)/Code/FSRDetectorCode/FSRDetectorCode.ino
+++ FSR (Force Sensitive Resistor)/Code/FSRDetectorCode/FSRDetectorCode.ino
@@ -1,20 +1,18 @@
- 
-
-int redLED = 13;
-int greenLED = 12; 
-int blueLED = 11;
+int redLED = 6;
+int greenLED = 5; 
+int blueLED = 3;
 int FSRpin = 7; 
-int fsrReading; 
+int fsrReading;   
 int fsrMVoltage; 
-int lowRange = 300; 
-int highRange = 500; 
+float lowRange = 400; 
+float highRange = 700; 
 float fsrVoltage; 
-int sum; 
-int loopNum; 
-int fsrAvgReading; 
-
+float fsrMass; 
+float lowThreshold = 50; 
+float highThreshold = 900; 
 // setup function that runs once at the start of the code
-void setup() {
+void setup() { 
+  
   // initialize the digital pin as an output.
     Serial.begin(9600); 
 
@@ -22,79 +20,68 @@ void setup() {
   pinMode(redLED, OUTPUT);
   pinMode(greenLED, OUTPUT); 
   pinMode(blueLED, OUTPUT);
-  
-  
 }
 
 // loop function that runs repeatedly
 void loop() {
-  
+
 //   gets the fsr reading (0-1023)
-  sum = 0 ;
-  loopNum = 10; 
-//  fsrReading = analogRead(A1); 
-
-  for (int i=0; i<loopNum; i++){
-    fsrReading = analogRead(A1); 
-    sum += fsrReading ;
-  }
-
-  fsrAvgReading = sum/loopNum;
-  Serial.println("=====================\n=====================");   
-  Serial.print("Raw Reading: ") ;
-  Serial.println(fsrAvgReading);   
-
+  fsrReading = analogRead(A1); 
+  Serial.println("=====================");   
+  Serial.print("Raw Reading:: ") ;
+  Serial.println(fsrReading);   
   
-//  conditionals for determining LEDS to turn on.  
-  if (fsrReading < lowRange){
-   turnOffAll();
-  turnOnRed() ;
-  }
-  else if (fsrReading > lowRange && fsrReading < highRange){
-  turnOffAll();
-  turnOnGreen();
-  }
-  else {
-    turnOffAll();
-    turnOnBlue; 
-  }
-
-  
-  
-  
-  //converting the fsr reading into millivolts  and printing 
- 
-  fsrMVoltage = map(fsrAvgReading, 0, 1023, 0, 5000);
+//converting the fsr reading into millivolts  and printing to serial console 
+  fsrMVoltage = map(fsrReading, 0, 1023, 0, 5000);
   fsrVoltage = fsrMVoltage / 1000.000; 
   Serial.print("Voltage reading in ");
   Serial.println(fsrVoltage, 4);  
   Serial.print("Predicted Mass:"); 
-  Serial.println(predictMass(fsrVoltage)); 
-  delay(1500);
+  fsrMass = predictMass(fsrVoltage);
+  Serial.println(fsrMass); 
+
   
-}
-
-void turnOnRed (){
-  digitalWrite(redLED, HIGH);
-}
-void turnOnGreen (){
+//  conditionals for determining LEDS to turn on.  
+  if (fsrMass < lowThreshold || fsrMass > HighThreshold){
+    turnOffAll();
+  }
+ else if (fsrMass < lowRange){
+    Serial.print("In low range ");
+   turnOffAll();
+  digitalWrite(redLED, HIGH) ;
+  }
+  else if (fsrMass < highRange){
+    Serial.print("In mid range ");
+  turnOffAll();
   digitalWrite(greenLED, HIGH);
-}
-void turnOnBlue (){
-  digitalWrite(blueLED, HIGH);
+  }
+  else {
+    Serial.print("In high range in ");
+    turnOffAll();
+    digitalWrite(blueLED, HIGH); 
+  }
+
+    delay(10000);
+
 }
 
+//function to turn off LED
 void turnOffAll () { 
-  digitalWrite(redLED, LOW); 
+  digitalWrite(redLED, LOW);  
   digitalWrite(greenLED, LOW); 
   digitalWrite(blueLED, LOW); 
 }
 
+// return predicted mass based on voltage 
 float predictMass(float voltage) {
 
-//  return (pow(log(a * voltage * 1000), d))/b;
-return pow(10, (0.3465*voltage + 1.283437)); 
+  // formulas to convert voltage into grams, depending on the range of the voltage 
+if (voltage < 1.2) {  return pow(10, (0.6377551*voltage + 1.12818877)); }
 
-//return (pow(2.718, (2.254*voltage)-.2254)-1)/0.38;
+else if (voltage < 2.35) {return pow(10, (0.18188432*voltage + 1.58875954)); }
+
+ else if (voltage < 4.1) {return pow(10, (0.37341299*voltage + 1.07318894)); }
+
+else{ {return pow(10, (0.7800312*voltage -0.66357254));}
 }
-     
+}
