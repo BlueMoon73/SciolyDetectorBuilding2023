8ab513ab8f5cff473140f6f683fba93f759dec82
Monish Saravana <monish.sd.2008@Gmail.com>
2023-02-22 00:01:12 -0500

    Added working load cell code
    Working code, to read value from load cell

--- Load Cell/Code/loadcellcode/loadcellcode.ino
+++ Load Cell/Code/loadcellcode/loadcellcode.ino
@@ -1,32 +1,19 @@
-// Calibrating the load cell
-#include <HX711.h>
-// HX711 circuit wiring
-const int LOADCELL_DOUT_PIN = 2;
-const int LOADCELL_SCK_PIN = 3;
+float  val = 0; 
+float analval = 0; 
+int DOUT = 3; 
+int SCKpin = 2; 
+void setup(){
 
-HX711 scale;
+  Serial.begin(9600);
+  pinMode(DOUT, INPUT); 
 
-void setup() {
-  Serial.begin(57600 );
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
 }
 
-void loop() {
+void loop(){
+ val = digitalRead(DOUT); 
+ analval = analogRead(DOUT);
+ Serial.println( val); 
+ Serial.println(analval); 
+ delay(1000);
 
-  if (scale.is_ready()) {
-    scale.set_scale();    
-    Serial.println("Tare... remove any weights from the scale.");
-
-    scale.tare();
-    Serial.println("Tare done...");
-    Serial.print("Place a known weight on the scale...");
-   
-    long reading = scale.get_units(10);
-    Serial.print("Result: ");
-    Serial.println(reading);
-  } 
-  else {
-    Serial.println("HX711 not found.");
-  }
-  delay(1000);
 }
