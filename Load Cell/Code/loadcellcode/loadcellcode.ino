199a4a2bb561161d43646bb16b76db1a25ce07ed
Monish Saravana <monish.sd.2008@Gmail.com>
2023-02-22 19:28:46 -0500

    Implemented the HX711
    Implemented HX711 library for reading values

--- Load Cell/Code/loadcellcode/loadcellcode.ino
+++ Load Cell/Code/loadcellcode/loadcellcode.ino
@@ -1,19 +1,36 @@
+#include "HX711.h"
+HX711 loadcell;
+
+
 float  val = 0; 
 float analval = 0; 
-int DOUT = 3; 
+int DOUTpin = 3; 
 int SCKpin = 2; 
+
+const long LOADCELL_OFFSET = 50682624;
+const long LOADCELL_DIVIDER = 5895655;
+
+
 void setup(){
 
-  Serial.begin(9600);
-  pinMode(DOUT, INPUT); 
 
+  loadcell.begin(DOUTpin, SCKpin);
+  Serial.begin(9600);
+  pinMode(DOUTpin, INPUT); 
+loadcell.set_scale(LOADCELL_DIVIDER);
+loadcell.set_offset(LOADCELL_OFFSET);
 }
 
 void loop(){
- val = digitalRead(DOUT); 
- analval = analogRead(DOUT);
+ val = digitalRead(DOUTpin); 
+ analval = analogRead(DOUTpin);
+ Serial.print("Digital Val Reading"); 
  Serial.println( val); 
+ Serial.print("Analog Val Reading"); 
  Serial.println(analval); 
+ // 4. Acquire reading
+ Serial.print("Weight: ");
+ Serial.println(loadcell.get_units(10), 2);
  delay(1000);
 
 }
